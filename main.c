10f6752b250e9971249e82b7823d4db145d977e0
j4ckalll <jishnuvinodgk@gmail.com>
2026-07-04 22:31:14 +0530

    print stars

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-    printf("Hello, guys!\n");
-    return 0;
+int main() 
+{
+  int i,j,a;
+  printf("enter a number :");
+    scanf("%d",&a);
+  for(i=0;i<=a;i++)
+  {
+    for(j=0;j<i;j++)
+    {
+    printf("*");  
+    }
+    printf("\n");
+  }
+   
 }
+
