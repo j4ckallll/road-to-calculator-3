074461a417e95b3875a71f0066c5324ff7014081
j4ckalll <jishnuvinodgk@gmail.com>
2026-07-04 23:16:01 +0530

    enter the size of the array

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 int main() 
 {
-  int a[5],i;
-  printf("Enter the values :\n");
-  for(i=0;i<5;i++)
+  int a[10000],i;
+  int limit;
+  printf("Enter array limit : ");
+  scanf("%d",&limit);
+  printf("enter the values to store in array :\n");
+  for(i=0;i<limit;i++)
   {
     scanf("%d",&a[i]);
   }
   printf("Entered values are :  ");
-  for(i=0;i<5;i++)
+  for(i=0;i<limit;i++)
   {
     printf("%d \t",a[i]);
    } 
