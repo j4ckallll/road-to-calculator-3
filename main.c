e0c60160305b0507259e87394e9d0ff9e5abb024
j4ckalll <jishnuvinodgk@gmail.com>
2026-07-07 18:29:49 +0530

    print char

--- main.c
+++ main.c
@@ -2,31 +2,11 @@
 #include <stdlib.h>
 int main() 
 {
-  int limit,arr[1000],i,search,flag=0,j,temp;
-  printf("Enter the the limit :");
-  scanf("%d",&limit);
-   printf("enter the value :\n" );
-  for (i=0;i<limit;i++)
-  {
-    scanf("%d", &arr[i]);
-  }
-  for(i=0;i<limit-1;i++)
-  {
-    for(j=i+1;j<limit;j++)
-    {
-      if(arr[i]>arr[j])
-      {
-      temp=arr[j];
-      arr[j]=arr[i];
-      arr[i]=temp;
-      } 
-    }
-  }
-  printf("sorted value :\n");
-  for(i=0;i<limit;i++)
-  {
-  printf(" %d\t",arr[i]);
-  }
+  char name[20];
+  printf("Enter the name : ");
+  scanf("%s",name);
+  printf("entered name is : %s",name);
+
   
 }
 
